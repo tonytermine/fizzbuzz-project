517faf60fc5c714adaaabfdfbe0297634d436217
Tony Termine <termine.tony@gmail.com>
2014-04-19 14:32:54 -0400

    Code refactor
    Added reset input function.  cleaned up un-needed code.  added comments
    to provide clarity.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
-// append a new number to the list by taking the value of the user input when they click on the enter button. Have an alert pop up if user does not enter anything in the input field.  Must be a number from 1-100.
 var fizz = ("Fizz");
 var buzz = ("Buzz");
 var fizzBuzz = ("Fizz-Buzz");
 var validInput = true;
-var resetNewEntry = function () {
+var resetList = function () {
 	$(".list").empty();
 };
+var resetInputBar = function () {
+	$(".main-input").val("");
+};
+// append a new number to the list by taking the value of the user input when they click on the enter button. Must be a number from 1-100. Clear list everytime a new number is entered and clear input field after each entry.
 $(".enter-button").click(function(e) {
 	var toAdd = $(".main-input").val();
 	e.preventDefault();
-	resetNewEntry();
-	if(toAdd != "" && $.isNumeric(toAdd)) {
-		validInput = true;
+	resetList();
+// Have an alert pop up if user does not enter anything in the input field, enters a space, letter, or decimal.
+	if(toAdd != "" && $.isNumeric(toAdd) && toAdd % 1 == 0) {
 	} else {
-		validInput = false;
-	}/*end if statement*/
+		alert("Please, enter number to play Fizz-Buzz");
+	}/*end alert*/
 
-	if(validInput) {
 	for (x = 1; x <= toAdd; x++) {
   if (x % 3 == 0 && x % 5 == 0) {
 		$(".list").append("<li class='fizz-buzz'>" + fizzBuzz + "</li>");
@@ -28,14 +30,11 @@ $(".enter-button").click(function(e) {
 		$(".list").append("<li>"+ x +"</li>");
 	}/*end if/else series*/
 	}/*end for loop*/
-	}/*end validInput*/else {
-		alert("Please, enter number to play Fizz-Buzz");
-	}
-	$(".main-input").val(""); // reset input
-});/*end click function*/
+	resetInputBar();
+});/*end enter-button click function*/
 
 // clear entire list by clicking reset button
 $(".reset-button").click(function(e) {
-	resetNewEntry();
+	resetList();
 	e.preventDefault();
-});/*end of reset click event*/
+});/*end of reset-button click event*/
